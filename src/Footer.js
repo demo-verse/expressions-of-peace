8e55a303d2197ef5c5e67cc2eee8020286b23a5e
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-09-28 19:13:09 +0300

    add demoVerse discord as community

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,41 @@ const Footer = () => {
 return (
     <footer>
         <div className="container">
+
+          <a
+            className="source-code-link"
+            href="https://discord.gg/CfGzp3st3k"
+            rel="noreferrer"
+            target="_blank"
+            style={{fontSize: "1.2rem"}}
+          >
+            community
+          </a>
+
+          <a
+            className="source-code-link"
+            href="https://app.dework.xyz/demoverse/expressions-of-peace/view/board-l821cjm7"
+            rel="noreferrer"
+            target="_blank"
+            style={{fontSize: "1.2rem"}}
+          >
+            projection
+          </a>
+         
+          <a
+            className="source-code-link"
+            href="https://github.com/demo-verse/expressions-of-peace"
+            rel="noreferrer"
+            target="_blank"
+            style={{fontSize: "1.2rem"}}
+          >
+            sourceCode
+          </a>
+       
+          {/* {gasPrice} gwei &bull; {blockNumber} */}
+        </div>
+
+        <div className="columns">
           <a
             className="license-link"
             href="https://creativecommons.org/licenses/by-sa/2.0/"
@@ -14,65 +49,6 @@ return (
               alt="creative commons shareAlike"
             ></img>
           </a>
-          {/* expressions:
-          <a
-            className="contract-at-goerli"
-            href="https://goerli.etherscan.io/address/0xe563950e3d97c1cf11665163d4b14ead092c503c"
-            rel="noreferrer"
-            target="_blank"
-          >
-            @Görli
-          </a>
-          <a
-            className="contract-at-rinkeby"
-            href="https://rinkeby.etherscan.io/address/0x6d584295790d2c9f7f2d4249b6caebc15b1da682"
-            rel="noreferrer"
-            target="_blank"
-          >
-            @Rinkeby
-          </a>
-          faucets:
-          <a
-            className="faucet-goerli"
-            href="https://goerlifaucet.com/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            $Görli
-          </a>
-          <a
-            className="faucet-rinkeby"
-            href="https://faucet.rinkeby.io/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            $Rinkeby
-          </a> */}
-          <a
-            className="source-code-link"
-            href="https://github.com/demo-verse/expressions-of-peace"
-            rel="noreferrer"
-            target="_blank"
-            style={{fontSize: "1.2rem"}}
-          >
-            sourceCode
-          </a>
-
-          <a
-            className="source-code-link"
-            href="https://app.dework.xyz/demoverse/expressions-of-peace"
-            rel="noreferrer"
-            target="_blank"
-            style={{fontSize: "1.2rem"}}
-          >
-            contribute
-          </a>
-
-       
-          {/* {gasPrice} gwei &bull; {blockNumber} */}
-        </div>
-
-        <div className="columns">
         <a
             // className="source-code-link"
             href="https://demover.se"
@@ -80,7 +56,7 @@ return (
             target="_blank"
             style={{fontSize: "1.2rem"}}
           >
-             <img src="demoVerse-logo.png" width={"100px"} height="32px" alt="demoVerse">
+             <img src="demoVerse-logo.png" width={"100px"} height="38px" alt="demoVerse">
           </img>
           </a>
 
@@ -91,9 +67,10 @@ return (
             target="_blank"
             style={{fontSize: "1.2rem"}}
           >
-             <img src="start-logo.png" width={"100px"} height="32px" alt="stateful.art">
+             <img src="start-logo.png" width={"100px"} height="38px" alt="stateful.art">
           </img>
           </a>
+
         </div>
       </footer>
 );
